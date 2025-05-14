b267602744431be15e879243503a9f59dba73096
AzureAD\moritaizo <mori.taizo@blueship.co.jp>
2025-05-14 11:02:43 +0900

    新規コンポーネントMyPage.vue作成　router定義

---
+++ task_checker_front/src/components/MyPage.vue
@@ -0,0 +1,16 @@
+//以下すべてオリジナルです 森
+
+<script setup>
+import Task from './Task.vue'
+import { ref, onMounted } from 'vue'
+import api from '../api/axios'
+
+onMounted(async() => {
+  const response = await api.get('/tasks');
+  tasks.value = response.data;
+})
+</script>
+
+<template>
+  <Task>aaaaa</Task>
+</template>
